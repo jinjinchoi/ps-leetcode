0ca7f0b7aa2aac79d4ce2c540ce87d1e54f21032
Jung In Choi <luvimperfection@gmail.com>
2021-10-11 07:31:11 +0900

    Time: 332 ms (25.00%), Space: 37.1 MB (25.00%) - LeetHub

--- bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.kt
+++ bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.kt
@@ -1,16 +1,16 @@
 class Solution {
     fun rangeBitwiseAnd(left: Int, right: Int): Int {
         // finding common prefix of left & right
-        var shift = 1
+        var shift = 0
         var from = left
         var to = right
         
         while(from != to){
             from = from shr 1
             to = to shr 1
-            shift = shift shl 1
+            shift += 1
         }
         
-        return shift * from
+        return from shl shift
     }
 }

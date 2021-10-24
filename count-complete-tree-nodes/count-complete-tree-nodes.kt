0d9713f0a2ee23d51cd2637b0a299939a8b2f9dd
Jung In Choi <luvimperfection@gmail.com>
2021-10-25 07:02:20 +0900

    Time: 383 ms (24.44%), Space: 44.7 MB (53.33%) - LeetHub

--- count-complete-tree-nodes/count-complete-tree-nodes.kt
+++ count-complete-tree-nodes/count-complete-tree-nodes.kt
@@ -1,16 +1,13 @@
-/**
- * Example:
- * var ti = TreeNode(5)
- * var v = ti.`val`
- * Definition for a binary tree node.
- * class TreeNode(var `val`: Int) {
- *     var left: TreeNode? = null
- *     var right: TreeNode? = null
- * }
- */
 class Solution {
     fun countNodes(root: TreeNode?): Int {
         if(root==null) return 0
-        return 1 + countNodes(root.left) + countNodes(root.right)
+        val height = calculateHeight(root)
+        val rightHight = calculateHeight(root.right)
+        if (rightHight == (height - 1) ) return (1 shl height-1) + countNodes(root.right) 
+        else return (1 shl height-2) + countNodes(root.left)
+    }
+    
+    fun calculateHeight(root: TreeNode?): Int{
+        return if (root==null)  0 else 1 + calculateHeight(root.left)
     }
 }

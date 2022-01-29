4225d55edc13e8f8a25bf6e0a79a2a4347f71f6a
JungIn Choi <luvimperfection@gmail.com>
2022-01-29 11:07:52 +0900

    디렉토리 구조 변경, hasty-java 추가

---
+++ hasty-java/src/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    int[] personOnePattern = {1,2,3,4,5};
+    int[] personTwoPattern = {2, 1, 2, 3, 2, 4, 2, 5};
+    int[] personThreePattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+
+    // 수포자 1의 점수
+    public int getScorePersonOne(int[] answers){
+        int numQuestions = answers.length;
+        for(int i=0; i<numQuestions; i++){
+            answers[i]
+        }
+    }
+}

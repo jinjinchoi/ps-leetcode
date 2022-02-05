c28277889e98b3340984dec8570eb3f2f6c4079f
JungIn Choi <luvimperfection@gmail.com>
2022-02-05 16:17:54 +0900

    BFSDFS/타겟넘버/java/v2

--- hasty-java/src/BFSDFS/타겟넘버/BFSDFS02.java
+++ hasty-java/src/BFSDFS/타겟넘버/BFSDFS02.java
@@ -1,45 +1,29 @@
 package BFSDFS.타겟넘버;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
 public class BFSDFS02 {
+    int networkCount;
     public int solution(int n, int[][] computers) {
-        return getNetworkCount(getSquaredMatrix(computers));
-    }
-
-    public int[][] getSquaredMatrix(int[][] computers){
-        int size = computers.length;
-        int[][] squaredMatrix = new int[size][size];
-        for(int rowIndex=0; rowIndex<size; rowIndex++){
-            for(int colIndex=0; colIndex<size; colIndex++){
-                int element = 0;
-                for(int i=0; i<size; i++) element += computers[rowIndex][i]*computers[colIndex][i];
-                squaredMatrix[rowIndex][colIndex] = element;
+        networkCount = 0;
+        int[] marked = new int[n];
+        for(int row=0; row<n; row++){
+            if(marked[row]==0) {
+                networkCount += 1;
+                DFS(row, computers, marked);
             }
         }
-        return squaredMatrix;
+        return networkCount;
     }
 
-    public int getNetworkCount(int[][] squaredMatrix){
-        Set<Integer> networkSet = new HashSet<>();
-        int size = squaredMatrix.length;
-        HashMap<Integer,Integer> networkMap = new HashMap<>();
-        for(int rowIndex=0; rowIndex<size; rowIndex++){
-            for(int colIndex=0; colIndex<size; colIndex++){
-                if(networkMap.get(colIndex) == null && squaredMatrix[rowIndex][colIndex]>0) {
-                    networkMap.put(colIndex, rowIndex);
-                    networkSet.add(rowIndex);
-                }
-            }
+    public void DFS(int row, int[][] computers, int[] marked){
+        marked[row] = 1;
+        for(int col=0; col<computers.length; col++){
+            if(marked[col]==0 && computers[row][col]==1) DFS(col, computers, marked);
         }
-        return networkSet.size();
     }
+}
 
-    @Test
+class TestBFSDFS02{
+    @org.junit.jupiter.api.Test
     public void checkAnswer() {
         int numCount1 = 3;
         int[][] computers1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
@@ -53,14 +37,16 @@ public class BFSDFS02 {
         int numCount4 = 5;
         int[][] computers4 = {{1, 0, 0, 1, 0}, {0, 1, 1, 0, 0}, {0, 1, 1, 0, 0}, {1, 0, 0, 1, 0}, {0, 0, 0, 0, 1}};
 
-        int numCount5 = 5;
+        int numCount5 = 6;
         int[][] computers5 = {{1, 0, 0, 1, 0, 0}, {0, 1, 1, 0, 0, 0}, {0, 1, 1, 0, 0, 0}, {1, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}};
 
 
-        assert 2 == solution(numCount1, computers1);
-        assert 1 == solution(numCount2, computers2);
-        assert 2 == solution(numCount3, computers3);
-        assert 3 == solution(numCount4, computers4);
-        assert 4 == solution(numCount5, computers5);
+
+        BFSDFS02 sol = new BFSDFS02();
+        assert 2 == sol.solution(numCount1, computers1);
+        assert 1 == sol.solution(numCount2, computers2);
+        assert 2 == sol.solution(numCount3, computers3);
+        assert 3 == sol.solution(numCount4, computers4);
+        assert 4 == sol.solution(numCount5, computers5);
     }
 }

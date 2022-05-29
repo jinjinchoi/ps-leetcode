4f1667a90546425213d0f76a096577a74fb4e683
Jung In Choi <luvimperfection@gmail.com>
2022-05-30 08:19:51 +0900

    Time: 298 ms (84.50%), Space: 37 MB (80.23%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.kt
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.kt
@@ -4,21 +4,9 @@ class Solution {
         s.forEachIndexed{ i, targetChar ->
             charHash.put(targetChar, charHash.getOrPut(targetChar){0} + 1)
         }
-        // var index = -1
-        // for(i)
-        // charHash.forEach { char, count ->
-        //     if(count==1) return s.indexOf(char)
-        // }
-        // println("s:" + s)
         s.forEach{ char ->
             if(charHash.get(char) == 1) return s.indexOf(char)
         }
-        
-//         for ((char, count) in charHash) {
-//             println("char: "+char+" count: "+count)
-//             if(count==1) return s.indexOf(char)
-//         }
-        
         return -1
     }
 }

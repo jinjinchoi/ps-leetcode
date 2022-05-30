a09898b944776f376c5fac500d8e01d5d4c63b85
Jung In Choi <luvimperfection@gmail.com>
2022-05-31 07:46:02 +0900

    Time: 485 ms (23.21%), Space: 52.5 MB (33.79%) - LeetHub

--- 383-ransom-note/383-ransom-note.kt
+++ 383-ransom-note/383-ransom-note.kt
@@ -1,13 +1,15 @@
 class Solution {
     fun canConstruct(ransomNote: String, magazine: String): Boolean {
         var magazineHash = HashMap<Char, Int>()
+        var value = 0
         magazine.forEach{ c->
             magazineHash.put(c, magazineHash.getOrDefault(c, 0)+1)
         }
     
         ransomNote.forEach{ m->
-            if (magazineHash.getOrDefault(m, 0) == 0) return@canConstruct false
-            magazineHash.put(m, magazineHash.getOrDefault(m, 0)-1)
+            value = magazineHash.getOrDefault(m, 0)
+            if (value == 0) return@canConstruct false
+            magazineHash.put(m, value-1)
         }
         return true
     }

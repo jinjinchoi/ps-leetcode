5bab28cb41ba6f0a9e5c2d5929c5ebce3f273429
Jung In Choi <luvimperfection@gmail.com>
2022-06-02 07:51:13 +0900

    Time: 239 ms (63.36%), Space: 37 MB (32.82%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.kt
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun runningSum(nums: IntArray): IntArray {
-        nums.drop(1).forEachIndexed{ i, num ->
-            nums[i+1] = num + nums[i]
+        for(i in 1..nums.size-1){
+            nums[i] = nums[i-1] + nums[i]
         }
         return nums
     }

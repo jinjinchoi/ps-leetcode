27a660e4df45f13a39eeedbb8d6784b4b38dc162
Jung In Choi <luvimperfection@gmail.com>
2022-06-16 08:22:50 +0900

    Time: 2870 ms (9.09%), Space: 211.6 MB (63.64%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.rb
@@ -0,0 +1,28 @@
+# @param {String[]} words
+# @return {Integer}
+def longest_str_chain(words)
+    words_size = words.size
+    sorted_words =  words.sort_by {|x| x.length}
+    # puts sorted_words
+    result_hash = sorted_words.to_h { |x| [x, 1] }
+    
+    for i in 0..words_size-1 do
+       word =  sorted_words[i]
+        word.each_char.with_index do |char, index|
+           # puts "#{word}'s substr, index #{index}: #{word.slice(0, index)+word.slice(index+1, word.length)}"
+           sliced_word = word.slice(0, index)+word.slice(index+1, word.length)
+          if words.include?(sliced_word)
+              # puts "..is inside words! --- result_hash[sliced_word] #{result_hash[sliced_word]}, result_hash[word] #{result_hash[word]}"
+              result_hash[word] = [result_hash[sliced_word]+1, result_hash[word]].max
+              # puts "updated: "
+              # break
+          end
+        end
+
+        
+        
+    end
+    
+    
+    return result_hash.values.max
+end

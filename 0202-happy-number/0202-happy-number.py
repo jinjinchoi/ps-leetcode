e69403f0d9a371068f979c7f456bfcc877d612f9
Jung In Choi <luvimperfection@gmail.com>
2023-07-04 07:00:05 +0900

    Time: 51 ms (56.41%), Space: 16.4 MB (32.56%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,21 +1,16 @@
 class Solution:
             
-    def digitSum(self, n: int) -> int:
+    def digit_sum(self, n: int) -> int:
         sum = 0
-        # print("n: %d" % n)
         while n > 0:
-            sum += (n % 10)**2
-            n  = n // 10
-            
-        # print("sum: %d" % sum)
+            n, digit = divmod(n, 10)
+            sum += digit **2
         return sum
 
     def isHappy(self, n: int) -> bool:
-        digitSum = self.digitSum(n)
-        k = 0
-        while digitSum != 1:
-            digitSum = self.digitSum(digitSum)
-            if digitSum < 10 and digitSum !=1:
-                return False
+        seen_set = set()
+        while n != 1 and n not in seen_set:
+            seen_set.add(n)
+            n = self.digit_sum(n)
     
-        return True
+        return n == 1

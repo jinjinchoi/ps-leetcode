bfe41e702b455809ff64564cb128b94bdf755234
Jung In Choi <luvimperfection@gmail.com>
2023-07-05 07:07:31 +0900

    Time: 159 ms (23.64%), Space: 19 MB (76.96%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,18 @@
+from collections import defaultdict
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        num_list = []
-        for number in nums:
-            if number in num_list:
-                num_list.remove(number)
-            else:
-                num_list.append(number)
-        return num_list.pop()
+        # num_list = [] # space complexity O(n)
+        # for number in nums: # time complexity *n
+        #     if number in num_list: # time complexity *n
+        #         num_list.remove(number)
+        #     else:
+        #         num_list.append(number)
+        # return num_list.pop()
+        hash_table = defaultdict(int)
+        for number in nums: # time complexity n
+            hash_table[number] += 1
+        for key in hash_table: # time complexity n
+            if hash_table[key] == 1:
+                return key
+        

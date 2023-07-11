ddddb7b629c03f72c3093ba57bf401720628d2f7
Jung In Choi <luvimperfection@gmail.com>
2023-07-12 06:50:30 +0900

    Time: 55 ms (71.37%), Space: 16.6 MB (95.96%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,20 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums)-1
-        mid = (left+right)//2
         while left <= right:
             mid = (left+right)//2
             if nums[mid] > nums[-1]:
                 left = mid + 1
             else:
                 right = mid - 1
-        # target이 그럼 첫번째 애보다 작은지, 큰지를 보고.
-        # 첫번째 애보다 크면 k 이전
-        # 첫번째 애보다 작으면 k 이후 --> binary search!
-        # O logN이어야 한대
-        
-        
-        
         def binary_search(left, right, target):
             left, right = left, right
             while left <= right:
